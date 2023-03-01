ecae88055b3a41477b8d0a3e4f9e0552342e3517
Ignatev Nikita <nikitaignatiev@gmail.com>
2023-03-01 16:49:30 +0300

    Creating inline buttons and add collback function and reply to callback

--- app/store/vk_api/accessor.py
+++ app/store/vk_api/accessor.py
@@ -7,13 +7,70 @@ from aiohttp import TCPConnector
 from aiohttp.client import ClientSession
 
 from app.base.base_accessor import BaseAccessor
-from app.store.vk_api.dataclasses import Message, Update, UpdateObject
+from app.store.vk_api.dataclasses import (Message,
+                                          UpdateObject,
+                                          MessageEvent,
+                                          Update,
+                                          UpdateObjectMessageNew,
+                                          UpdateObjectMessageEvent,
+                                          )
 from app.store.vk_api.poller import Poller
 
 if typing.TYPE_CHECKING:
     from app.web.app import Application
 
 API_PATH = "https://api.vk.com/method/"
+
+KEYBOARD = {
+    "keyboard_default": {
+        "buttons": [
+            [
+                {
+                    "action": {
+                        "type": "text",
+                        "payload": "",
+                        "label": "/поехали"
+                    },
+                }
+            ],
+            [
+                {
+                    "action": {
+                        "type": "callback",
+                        "payload": "",
+                        "label": "/Правила"
+                    },
+                }
+            ]
+        ]
+    },
+    "keyboard_game": {
+        "buttons": [
+            [
+                {
+                    "action": {
+                        "type": "text",
+                        "payload": "",
+                        "label": "/приехали"
+                    },
+                }
+            ],
+            [
+                {
+                    "action": {
+                        "type": "callback",
+                        "payload": "",
+                        "label": "/Правила"
+                    },
+                }
+            ]
+        ]
+    },
+}
+MESSAGES_TYPE = {
+    "text": "message_new",
+    "event": "message_event"
+}
 
 
 class VkApiAccessor(BaseAccessor):
@@ -34,7 +91,7 @@ class VkApiAccessor(BaseAccessor):
         self.poller = Poller(app.store)
         self.logger.info("start polling")
         await self.poller.start()
-    
+
     async def disconnect(self, app: "Application") -> None:
         if self.session:
             await self.session.close()
@@ -51,12 +108,12 @@ class VkApiAccessor(BaseAccessor):
 
     async def _get_long_poll_service(self) -> None:
         async with self.session.get(
-            self._build_query(
-                host=API_PATH,
-                method="groups.getLongPollServer",
-                params={
-                    "group_id": self.app.config.bot.group_id,
-                    "access_token": self.app.config.bot.token,
+                self._build_query(
+                    host=API_PATH,
+                    method="groups.getLongPollServer",
+                    params={
+                        "group_id": self.app.config.bot.group_id,
+                        "access_token": self.app.config.bot.token,
                     },
                 )
         ) as resp:
@@ -66,19 +123,19 @@ class VkApiAccessor(BaseAccessor):
             self.server = data["server"]
             self.ts = data["ts"]
             self.logger.info(self.server)
-    
+
     async def poll(self) -> None:
         async with self.session.get(
-            self._build_query(
-                host=self.server,
-                method="",
-                params={
-                    "act": "a_check",
-                    "key": self.key,
-                    "ts": self.ts,
-                    "wait": 25,
-                },
-            )
+                self._build_query(
+                    host=self.server,
+                    method="",
+                    params={
+                        "act": "a_check",
+                        "key": self.key,
+                        "ts": self.ts,
+                        "wait": 25,
+                    },
+                )
         ) as resp:
             data = await resp.json()
             self.logger.info(data)
@@ -86,34 +143,75 @@ class VkApiAccessor(BaseAccessor):
             raw_updates = data.get("updates", [])
             updates = []
             for update in raw_updates:
-                updates.append(
-                    Update(
-                        type=update["type"],
-                        object=UpdateObject(
-                            id=update["object"]["message"]["id"],
-                            user_id=update["object"]["message"]["from_id"],
-                            text=update["object"]["message"]["text"],
-                            # peer_id=int(update["object"]["message"]["peer_id"]) - 2000000000,
-                            peer_id=int(update["object"]["message"]["peer_id"])
-                        ),
+                if update["type"] == MESSAGES_TYPE["text"]:
+                    updates.append(
+                        Update(
+                            type=MESSAGES_TYPE["text"],
+                            object_message_new=UpdateObjectMessageNew(
+                                id=update["object"]["message"]["id"],
+                                user_id=update["object"]["message"]["from_id"],
+                                peer_id=int(update["object"]["message"]["peer_id"]),
+                                text=update["object"]["message"]["text"],
+                            ),
+                            object_message_event=None
+                        )
                     )
-                )
-            await self.app.store.bots_manager.handle_updates(updates)
+
+                elif update["type"] == MESSAGES_TYPE["event"]:
+                    updates.append(
+                        Update(
+                            type=MESSAGES_TYPE["event"],
+                            object_message_event=UpdateObjectMessageEvent(
+                                id=update["event_id"],
+                                user_id=update["object"]["user_id"],
+                                peer_id=int(update["object"]["peer_id"]),
+                                event_id=update["object"]["event_id"],
+                                payload=update["object"]["payload"],
+                            ),
+                            object_message_new=None
+                        )
+                    )
+                await self.app.store.bots_manager.handle_updates(updates)
 
     async def send_message(self, message: Message) -> None:
         async with self.session.get(
-            self._build_query(
-                host=API_PATH,
-                method="messages.send",
-                params={
-                    # "user_id": message.user_id,
-                    "random_id": random.randint(1,  2 ** 32),
-                    "peer_id": message.peer_id,
-                    "message": message.text,
-                    "access_token": self.app.config.bot.token,
-                    "keyboard": message.keyboard,
-                },
-            )
+                self._build_query(
+                    host=API_PATH,
+                    method=message.method,
+                    params={
+                        # "user_id": message.user_id,
+                        "random_id": random.randint(1,  2 ** 32),
+                        "peer_id": message.peer_id,
+                        "message": message.text,
+                        "access_token": self.app.config.bot.token,
+                        "keyboard": json.dumps(KEYBOARD[message.keyboard_type])
+                    },
+                )
+        ) as resp:
+            data = await resp.json()
+            self.logger.info(data)
+
+    async def send_event(self, message_event: MessageEvent) -> None:
+        async with self.session.get(
+                self._build_query(
+                    host=API_PATH,
+                    method=message_event.method,
+                    # event_id=message_event.event_id,
+                    params={
+                        # "user_id": message.user_id,
+                        "access_token": self.app.config.bot.token,
+                        "event_id": message_event.event_id,
+                        "user_id": message_event.user_id,
+                        #"random_id": random.randint(1,  2 ** 32),
+                        "peer_id": message_event.peer_id,
+                        "event_data": json.dumps({
+                            "type": "show_snackbar",
+                            "text": message_event.message
+                        }),
+
+                        #"keyboard": json.dumps(KEYBOARD[message_event.keyboard_type])
+                    },
+                )
         ) as resp:
             data = await resp.json()
             self.logger.info(data)
--- app/store/vk_api/dataclasses.py
+++ app/store/vk_api/dataclasses.py
@@ -5,20 +5,42 @@ from dataclasses import dataclass
 class UpdateObject:
     id: int
     user_id: int
-    text: str
     peer_id: int
-    # chat_id: int
+
+
+@dataclass()
+class UpdateObjectMessageNew(UpdateObject):
+    text: str
+
+
+@dataclass
+class UpdateObjectMessageEvent(UpdateObject):
+    event_id: str
+    payload: str
 
 
 @dataclass
 class Update:
     type: str
-    object: UpdateObject
+    object_message_new: UpdateObjectMessageNew | None
+    object_message_event: UpdateObjectMessageEvent | None
 
 
 @dataclass
 class Message:
     # user_id: int
     text: str
-    keyboard: str
+    keyboard_type: str
     peer_id: int
+    method: str
+
+
+@dataclass
+class MessageEvent:
+    user_id: int
+    #text: str
+    #keyboard_type: str
+    event_id: str
+    peer_id: int
+    message: str
+    method: str
